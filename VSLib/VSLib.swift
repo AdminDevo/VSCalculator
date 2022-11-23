93bd8492d0470ab451c634d3fba5e2189745b22f
Dev <Devo>
2022-11-23 13:27:41 +0530

    Added test cases

---
+++ VSLib/VSLib.swift
@@ -0,0 +1,22 @@
+//
+//  VSLib.swift
+//  VSLib
+//
+//  Created by admin on 23/11/22.
+//
+
+import Foundation
+
+public final class VSLib {
+
+    let name = "VSLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
