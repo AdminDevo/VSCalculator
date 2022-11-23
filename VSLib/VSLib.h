36646d1b88899d638bc54bd7053ce4896ff33007
Dev <Devo>
2022-11-23 13:02:33 +0530

    Initial commit

---
+++ VSLib/VSLib.h
@@ -0,0 +1,18 @@
+//
+//  VSLib.h
+//  VSLib
+//
+//  Created by admin on 23/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VSLib.
+FOUNDATION_EXPORT double VSLibVersionNumber;
+
+//! Project version string for VSLib.
+FOUNDATION_EXPORT const unsigned char VSLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VSLib/PublicHeader.h>
+
+
